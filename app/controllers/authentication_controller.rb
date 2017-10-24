3a4e3f56c361bad3c1b640bb1c13934e7c3d3ef9
Victor Fernandes <victor.hmfd@gmail.com>
2017-10-24 13:01:38 -0200

    Create user_type attr
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,9 +3,10 @@ class AuthenticationController < ApplicationController
 
   def authenticate
     command = AuthenticateUser.call(params[:email], params[:password])
+    user = User.find_by_email(params[:email])
 
     if command.success?
-      render json: { auth_token: command.result }
+      render json: { auth_token: command.result , user_type: user.user_type}
     else
       render json: { error: command.errors }, status: :unauthorized
     end
