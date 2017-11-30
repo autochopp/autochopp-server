71d513d36697d6b4a707d2d5606c4a25f8721a5e
Victor Fernandes <victor.hmfd@gmail.com>
2017-11-30 16:35:01 -0200

    Creating tests for authentication controller
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe AuthenticationController, type: :controller do
 
+  describe "POST #authenticate" do
+    it "returns http status ok when user is valid" do
+      user = User.create(email: "teste@gmail.com", password: "123456")          
+      post :authenticate, params: {email: user.email, password: user.password}
+      expect(response).to have_http_status(:ok)      
+    end
+  end
+
+  describe "POST #authenticate" do
+    it "returns http status unauthorized when user is invalid" do
+      user = User.create(email: "teste@gmail.com", password: "123456")          
+      post :authenticate, params: {email: user.email, password: "654321"}
+      expect(response).to have_http_status(:unauthorized)      
+    end
+  end
+
 end
