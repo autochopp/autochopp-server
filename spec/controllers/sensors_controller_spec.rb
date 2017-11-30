64e47f5e57994e0989917600fb3442aef0c7f5e0
Victor Fernandes <victor.hmfd@gmail.com>
2017-11-30 16:14:22 -0200

    Creating tests for sensors controller
    Signed-off-by: Victor Fernandes <victor.hmfd@gmail.com>

--- spec/controllers/sensors_controller_spec.rb
+++ spec/controllers/sensors_controller_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe SensorsController, type: :controller do
 
+  describe "GET #getsensors" do
+    it "returns http status ok" do
+      get :get_sensors_values
+      expect(response).to have_http_status(:ok)      
+    end
+  end
+
+  describe "POST #setsensors" do
+    it "returns http status no_content" do
+      post :set_sensors_values, params: {temperature: "5", volume: "25000", machine_status: "true"}
+      expect(response).to have_http_status(:no_content)      
+    end
+  end
+
 end
